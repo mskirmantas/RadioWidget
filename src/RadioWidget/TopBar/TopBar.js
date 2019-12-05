0a2ec4c4d4e73001d77493fdf5df8bdb4a3a8420
mantasskirma <56951458+mantasskirma@users.noreply.github.com>
2019-12-05 12:48:33 +0200

    Removed action from "Switch" button

--- src/RadioWidget/TopBar/TopBar.js
+++ src/RadioWidget/TopBar/TopBar.js
@@ -3,16 +3,12 @@ import './TopBar.css';
 
 const TopBar = () => {
 
-    const reset = () => {
-        window.location.reload(false);
-    }
-
     return (
         <div className="TopBar"> 
             <div className="container nav">
                 <button className="btn-back" />
                 <h1>Stations</h1>
-                <button onClick={reset} className="btn-switch" />
+                <button className="btn-switch" />
             </div>
         </div>
     )
